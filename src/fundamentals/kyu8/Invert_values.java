7860fab5d9c4d7dc9062d5369bc39914c1187a68
Grafstain <grafstain@gmail.com>
2022-12-25 22:29:38 +0300

    the solution 1 array

--- src/fundamentals/kyu8/Invert_values.java
+++ src/fundamentals/kyu8/Invert_values.java
@@ -3,14 +3,18 @@ package fundamentals.kyu8;
 public class Invert_values {
 
     public static int[] invert(int[] array) {
-        int[] invertArray = new int[array.length];
-        for(int i=0; i<array.length;i++){
-            invertArray[i] = array[i]*(-1);
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= -1;
         }
-        return invertArray;
+        return array;
     }
+
     public static void main(String[] args) {
-        System.out.println(invert(new int[] {-1,-2,-3,-4,-5}));
+
+        int[] newArray = invert(new int[]{-1, -2, -3, -4, -5});
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(newArray[i]);
+        }
 
     }
 }
