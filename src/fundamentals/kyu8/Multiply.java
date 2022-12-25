52bce1f08a74293802de9202a31bcb6eaf5a47ba
Grafstain <grafstain@gmail.com>
2022-12-25 21:04:02 +0300

    the solution

---
+++ src/fundamentals/kyu8/Multiply.java
@@ -0,0 +1,13 @@
+package fundamentals.kyu8;
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply(2.0,4.0));
+
+
+    }
+}
