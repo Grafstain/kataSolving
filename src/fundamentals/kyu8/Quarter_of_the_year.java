b0152ec92656e6d64def7b0c6effa2eb44435148
Grafstain <grafstain@gmail.com>
2022-12-25 20:36:24 +0300

    clear solution month+2

--- src/fundamentals/kyu8/Quarter_of_the_year.java
+++ src/fundamentals/kyu8/Quarter_of_the_year.java
@@ -3,19 +3,7 @@ package fundamentals.kyu8;
 public class Quarter_of_the_year {
 
     public static int quarterOf(int month) {
-        // Your code here
-        if ((double)(month)/3<=1){
-            return 1;
-        }else
-        if ((double)(month)/3<=2){
-            return 2;
-        }else
-        if ((double)(month)/3<=3){
-            return 3;
-        }else{
-            return 4;
-        }
-
+        return (month+2)/3;
     }
     public static void main(String[] args) {
         System.out.println(quarterOf(3));
