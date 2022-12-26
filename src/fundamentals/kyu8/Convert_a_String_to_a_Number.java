1151c7922101e1be7a0f10e02560038e0702e587
Grafstain <grafstain@gmail.com>
2022-12-26 10:23:38 +0300

    parseInt solution

---
+++ src/fundamentals/kyu8/Convert_a_String_to_a_Number.java
@@ -0,0 +1,13 @@
+package fundamentals.kyu8;
+
+public class Convert_a_String_to_a_Number {
+
+    public static int stringToNumber(String str) {
+        //TODO: Convert str into a number
+        return Integer.parseInt(str);
+    }
+    public static void main(String[] args) {
+        System.out.println(stringToNumber("445"));
+
+    }
+}
