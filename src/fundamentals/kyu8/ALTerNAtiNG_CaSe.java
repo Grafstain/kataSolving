fd358dbf814b4d0e6ca287ef3495f95db3a0ca5d
Grafstain <grafstain@gmail.com>
2022-12-26 10:17:31 +0300

    for (char c : string.toCharArray()) solution

--- src/fundamentals/kyu8/ALTerNAtiNG_CaSe.java
+++ src/fundamentals/kyu8/ALTerNAtiNG_CaSe.java
@@ -6,17 +6,15 @@ public class ALTerNAtiNG_CaSe {
 
 
     public static String toAlternativeString(String string) {
-        // your code here!
-        String newString = "";
-        for(int i=0; i<string.length();i++){
-            if(string.substring(i,i+1).equals(string.substring(i,i+1).toUpperCase())){
-                 newString += string.substring(i,i+1).toLowerCase();
-            }else {
-                 newString += string.substring(i, i+1).toUpperCase();
+        String result = "";
+        for (char c : string.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                result += Character.toLowerCase(c);
+            } else {
+                result += Character.toUpperCase(c);
             }
         }
-        return newString;
-
+        return result;
     }
 
     public static void main(String[] args) {
