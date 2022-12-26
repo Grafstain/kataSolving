a1a818d0a28edb2fa33e677359068d75a8cd9302
Grafstain <grafstain@gmail.com>
2022-12-26 10:23:59 +0300

    parseInt solution

--- src/fundamentals/kyu8/Convert_a_String_to_a_Number.java
+++ src/fundamentals/kyu8/Convert_a_String_to_a_Number.java
@@ -3,7 +3,6 @@ package fundamentals.kyu8;
 public class Convert_a_String_to_a_Number {
 
     public static int stringToNumber(String str) {
-        //TODO: Convert str into a number
         return Integer.parseInt(str);
     }
     public static void main(String[] args) {
