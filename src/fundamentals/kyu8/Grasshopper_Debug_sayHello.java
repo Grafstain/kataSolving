ef15a023d29363505853a6f3c2cb58e5f5d705a0
Grafstain <grafstain@gmail.com>
2022-12-26 09:46:44 +0300

    the solution concat

---
+++ src/fundamentals/kyu8/Grasshopper_Debug_sayHello.java
@@ -0,0 +1,14 @@
+package fundamentals.kyu8;
+
+public class Grasshopper_Debug_sayHello {
+
+    public static String sayHello(String name) {
+
+        return "Hello, " + name;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(sayHello("Mr Spooke"));
+
+    }
+}
