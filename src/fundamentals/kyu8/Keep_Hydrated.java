3e4967e752f47545d5b4f11a76455cef41de3042
Grafstain <grafstain@gmail.com>
2022-12-27 16:15:32 +0300

    solution

---
+++ src/fundamentals/kyu8/Keep_Hydrated.java
@@ -0,0 +1,13 @@
+package fundamentals.kyu8;
+
+public class Keep_Hydrated {
+
+    public static int Liters(double time)  {
+        return (int)(time/2);
+
+    }
+    public static void main(String[] args) {
+        System.out.println(Liters(14.64));
+
+    }
+}
