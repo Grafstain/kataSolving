f911551a58d779e4b9eec35f626b7e5488116094
Grafstain <grafstain@gmail.com>
2022-12-27 16:00:47 +0300

    string.repeat solution

--- src/fundamentals/kyu8/String_repeat.java
+++ src/fundamentals/kyu8/String_repeat.java
@@ -2,11 +2,7 @@ package fundamentals.kyu8;
 
 public class String_repeat {
     public static String repeatStr(final int repeat, final String string) {
-        String result= "";
-        for(int i=0;i<=repeat;i++){
-            result += string;
-        }
-        return result;
+        return string.repeat(repeat);
     }
     public static void main(String[] args) {
         System.out.println(repeatStr(4, "a"));
