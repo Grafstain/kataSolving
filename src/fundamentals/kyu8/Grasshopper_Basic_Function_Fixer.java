80144bb75f13cc66f3cbe947bbe55241c281cdae
Grafstain <grafstain@gmail.com>
2022-12-27 16:03:41 +0300

    solution

---
+++ src/fundamentals/kyu8/Grasshopper_Basic_Function_Fixer.java
@@ -0,0 +1,12 @@
+package fundamentals.kyu8;
+
+public class Grasshopper_Basic_Function_Fixer {
+    public static int addFive(int num) {
+        int total = num + 5;
+        return total;
+    }
+    public static void main(String[] args) {
+        System.out.println(addFive(4));
+
+    }
+}
