6e14bedcc0bb0dd9493558e27e380b924a0bd455
Grafstain <grafstain@gmail.com>
2022-12-27 15:47:27 +0300

    solution

---
+++ src/fundamentals/kyu8/Basic_variable_assignment.java
@@ -0,0 +1,8 @@
+package fundamentals.kyu8;
+
+public class Basic_variable_assignment {
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+
+}
