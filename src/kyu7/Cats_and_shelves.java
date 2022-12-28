d3db7f65485408aa52514da9ac995e3ff58ac34b
Grafstain <grafstain@gmail.com>
2022-12-28 13:17:02 +0300

    one line solution

--- src/kyu7/Cats_and_shelves.java
+++ src/kyu7/Cats_and_shelves.java
@@ -2,10 +2,7 @@ package kyu7;
 
 public class Cats_and_shelves {
     public static int solution(int start, int finish) {
-        if((finish-start)%3==1){return (finish-start)/3+1;}
-        if((finish-start)%3==0){return (finish-start)/3;}
-        if((finish-start)%3==2){return (finish-start)/3+2;}
-        return 0;
+        return (finish - start)/3 + (finish - start) % 3;
     }
     public static void main(String[] args) {
         System.out.println(solution(1, 5));//2
